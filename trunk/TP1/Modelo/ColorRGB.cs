9326e2ce01ee8d256763321aaebcc8e0e5a8d097
carogalli <carogalli@07b75908-302f-5e7f-fc1a-0c613c11b170>
2010-04-11 15:23:08 +0000

    Rellenar

---
+++ trunk/TP1/Modelo/ColorRGB.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SistemasGraficos.Entidades
+{
+    public class ColorRGB
+    {
+        private int red;
+        private int green;
+        private int blue;
+
+        public int Red
+        {
+            set { this.red = value; }
+            get { return this.red; }
+        }
+        public int Green
+        {
+            set { this.green = value; }
+            get { return this.green; }
+        }
+        public int Blue
+        {
+            set { this.blue = value; }
+            get { return this.blue; }
+        }
+
+        public ColorRGB(int r, int g, int b)
+        {
+            this.Red = r;
+            this.Blue = b;
+            this.Green = g;
+        }
+    }
+}
