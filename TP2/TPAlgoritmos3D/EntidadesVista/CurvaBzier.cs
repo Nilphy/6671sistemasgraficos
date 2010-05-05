83e9ea7d1d89c07a8ebbc252fda737c5e6b1ac89
carogalli <carogalli@07b75908-302f-5e7f-fc1a-0c613c11b170>
2010-05-05 02:52:35 +0000

    CurvaBzier -> lo probé en otro proyecto que tengo en mi pc y tiré las clases, por ahí falta algo pero creo que esto es todo lo que modifique... la bspline sale parecido

---
+++ TP2/TPAlgoritmos3D/EntidadesVista/CurvaBzier.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SistemasGraficos.Entidades
+{
+    public class CurvaBzier
+    {
+        // Tienen que venir ordenados!!
+        private IList<PuntoFlotante> puntosControl;
+
+        public IList<PuntoFlotante> PuntosControl
+        {
+            set
+            {
+                if (value.Count % 4 != 0) throw new InvalidOperationException("Esto es para curvas de segmentos de cuatro puntos");
+
+                this.puntosControl = value;
+            }
+            get
+            {
+                return this.puntosControl;
+            }
+        }
+        
+        public CurvaBzier(IList<PuntoFlotante> puntosControl)
+        {
+            this.PuntosControl = puntosControl;
+        }
+
+        public double GetAlturaAtU(double u)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<PuntoFlotante> GetPuntosDiscretos(double deltaU)
+        {
+            IList<PuntoFlotante> puntosDiscretizados = new List<PuntoFlotante>();
+            
+            int numeroSegmento = 1;
+            while (numeroSegmento <= this.GetCantidadSegmentos())
+            {
+                IList<PuntoFlotante> puntosParteCurva = new List<PuntoFlotante>();
+
+                // TODO: Desharcodear el cuatro de acá
+                for (int i = 0; i < 4; i++)
+                {
+                    puntosParteCurva.Add(this.PuntosControl[numeroSegmento - 1 + i]);
+                }
+
+                for (double u = 0; u <= 1; u += deltaU)
+                {
+                    puntosDiscretizados.Add(this.GetValorEnSegmento(puntosParteCurva, u));
+                }
+
+                numeroSegmento++;
+            }
+
+            return puntosDiscretizados;
+        }
+
+        /// <summary>
+        /// Devuelve el valor de la curva Bziel cuyo polígono de control está formado
+        /// por los trés puntos pasados por parámetro, para el parámetro u e (0,1)
+        /// 
+        /// Formula sacada de la wikipedia 
+        /// B(t) = p0*((1-t)^3) + 3*P1*t*((1-t)^2) + 3*p2*(t^2)*(1-t) + p3*t^3
+        /// http://es.wikipedia.org/wiki/Curva_de_B%C3%A9zier
+        /// </summary>       
+        private PuntoFlotante GetValorEnSegmento(IList<PuntoFlotante> puntosParteCurva, double u)
+        {
+            if (puntosParteCurva.Count != 4) throw new InvalidOperationException("es una Bziel de 3 puntos de control");
+
+            PuntoFlotante primerSumando = puntosParteCurva[0].MultiplicarEscalar(Math.Pow(1-u, 3));
+            PuntoFlotante segundoSumando = puntosParteCurva[1].MultiplicarEscalar(3).MultiplicarEscalar(u).MultiplicarEscalar(Math.Pow(1 - u, 2));
+            PuntoFlotante tercerSumando = puntosParteCurva[2].MultiplicarEscalar(3).MultiplicarEscalar(Math.Pow(u, 2)).MultiplicarEscalar(1 - u);
+            PuntoFlotante cuartoSumando = puntosParteCurva[3].MultiplicarEscalar(Math.Pow(u, 3));
+
+            return primerSumando.SumarPunto(segundoSumando.SumarPunto(tercerSumando.SumarPunto(cuartoSumando)));
+        }
+
+        private int GetCantidadSegmentos()
+        {
+            return this.PuntosControl.Count / 4;
+        }
+    }
+}
--- TP2/TPAlgoritmos3D/EntidadesVista/PuntoFlotante.cs
+++ TP2/TPAlgoritmos3D/EntidadesVista/PuntoFlotante.cs
@@ -7,39 +7,32 @@ namespace SistemasGraficos.Entidades
 {
     public class PuntoFlotante : Punto
     {
-        private double x;
-        private double y;
-
-        public double X
+        private float x;
+        private float y;
+        
+        public float X
         {
             set { this.x = value; }
             get { return this.x; }
         }
-        public double Y
+        public float Y
         {
             set { this.y = value; }
             get { return this.y; }
         }
 
-        public PuntoFlotante(double x, double y)
+        public PuntoFlotante(float x, float y)
         {
             this.X = x;
             this.Y = y;
         }
 
-        public PuntoFlotante(double x, double y, Punto origenCoordenadas)
-        {
-            this.X = x;
-            this.Y = y;
-            this.OrigenCoordenadas = origenCoordenadas;
-        }
-
-        public override double GetXFlotante()
+        public override float GetXFlotante()
         {
             return this.X;
         }
 
-        public override double GetYFlotante()
+        public override float GetYFlotante()
         {
             return this.Y;
         }
@@ -52,6 +45,26 @@ namespace SistemasGraficos.Entidades
         public override int GetYEntero()
         {
             return (int)Math.Round(this.y, MidpointRounding.ToEven);
+        }
+
+        /// <summary>
+        /// No modifica el valor de this... devuelve un nuevo punto flotante con el valor multiplicado
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns> 
+        public PuntoFlotante MultiplicarEscalar(double p)
+        {
+            return new PuntoFlotante((float)(this.GetXFlotante() * p), (float)(this.GetYFlotante() *p));
+        }
+        
+        /// <summary>
+        /// No modifica el valor de this, ni del otro, devuelve un nuevo punto flotante con el valor de la suma de ambos
+        /// </summary>
+        /// <param name="punto"></param>
+        /// <returns></returns>
+        public PuntoFlotante SumarPunto(PuntoFlotante punto)
+        {
+            return new PuntoFlotante(this.GetXFlotante() + punto.GetXFlotante(), this.GetYFlotante() + punto.GetYFlotante());
         }
     }
 }
