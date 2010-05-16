fc584fe7d16fcfb2b5b64339f075ea8d5927c7b0
owentar <owentar@07b75908-302f-5e7f-fc1a-0c613c11b170>
2010-05-16 16:49:03 +0000

    - Refactor sobre el código para limpiar y modularizarlo un poco.

---
+++ TP2/TPAlgoritmos3D/EntidadesVista/CamaraPathPlotter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SistemasGraficos.Entidades;
+
+using Tao.OpenGl;
+
+namespace TPAlgoritmos3D.EntidadesVista
+{
+    public class CamaraPathPlotter
+    {
+        /// <summary>
+        /// Dibuja la curva correspondiente a la trayectoria de la camara. La misma
+        /// se encuentra definida por los puntos de control indicados por parámetros.
+        /// </summary>
+        /// <param name="puntos">Puntos de control</param>
+        public static void DibujarCamaraPath(IList<PuntoFlotante> puntos)
+        {
+            // Dibujo el lazo punteado que une los puntos.
+            PlotHelper.DibujarLazoPunteado(puntos);
+
+            // Dibujo los puntos para denotar su ubicación.
+            foreach (PuntoFlotante punto in puntos)
+            {
+                PlotHelper.DibujarPunto(punto);
+            }
+
+            // Dibujo la curva que se forma a partir de los puntos.
+            if (puntos.Count > 4)
+            {
+                CurvaBsplineSegmentosCubicos curvaCamino = new CurvaBsplineSegmentosCubicos(puntos);
+
+                Gl.glDisable(Gl.GL_LIGHTING);
+                Gl.glBegin(Gl.GL_LINE_STRIP);
+                Gl.glPushMatrix();
+                Gl.glColor3d(0.5, 0.5, 0);
+                foreach (PuntoFlotante punto in curvaCamino.GetPuntosDiscretos(0.001))
+                {
+                    Gl.glVertex2d(punto.X, punto.Y);
+                }
+                Gl.glPopMatrix();
+                Gl.glEnd();
+                Gl.glEnable(Gl.GL_LIGHTING);
+                Gl.glColor3d(1, 1, 1);
+            }
+        }
+
+    }
+}
---
+++ TP2/TPAlgoritmos3D/EntidadesVista/PlotHelper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Tao.OpenGl;
+using SistemasGraficos.Entidades;
+
+namespace TPAlgoritmos3D.EntidadesVista
+{
+    public class PlotHelper
+    {
+
+        public static void DibujarLazoPunteado<T>(IList<T> puntos) where T : Punto
+        {
+            Gl.glEnable(Gl.GL_LINE_STIPPLE);
+            Gl.glDisable(Gl.GL_LIGHTING);
+            Gl.glLineStipple(4, 0xAAAA);
+            Gl.glBegin(Gl.GL_LINE_STRIP);
+            Gl.glPushMatrix();
+
+            foreach (Punto punto in puntos)
+            {
+                Gl.glVertex2d(punto.GetXFlotante(), punto.GetYFlotante());
+            }
+
+            Gl.glPopMatrix();
+            Gl.glEnd();
+            Gl.glDisable(Gl.GL_LINE_STIPPLE);
+            Gl.glEnable(Gl.GL_LIGHTING);
+        }
+
+        /// <summary>
+        /// Dibuja un punto de control de la curva.
+        /// </summary>
+        /// <param punparam name="punto">Punto a ser dibujado.</param>
+        public static void DibujarPunto(Punto punto)
+        {
+            // TODO: ver de sacar este parametro a traves de la configuracion de la vista.
+            double DELTA = 0.01;
+            Gl.glPushMatrix();
+
+            Gl.glDisable(Gl.GL_LIGHTING);
+            Glu.GLUquadric quad = Glu.gluNewQuadric();
+            Gl.glPushMatrix();
+            Gl.glColor3d(1, 0, 0);
+            Gl.glTranslated(punto.GetXFlotante(), punto.GetYFlotante(), 0);
+            Glu.gluDisk(quad, 0, DELTA, 20, 20);
+            Gl.glPopMatrix();
+            Gl.glEnable(Gl.GL_LIGHTING);
+            Gl.glColor3d(1, 1, 1);
+            Glu.gluDeleteQuadric(quad);
+        }
+
+    }
+}
