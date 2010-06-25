4f7d7d9b95aaff9e2303335ffabbff20da92b090
damianferrai <damianferrai@07b75908-302f-5e7f-fc1a-0c613c11b170>
2010-06-25 01:36:02 +0000

    Arbolitos listos, pero sin lighting

--- TP3/Trochita3D/Entidades/Arbol.cs
+++ TP3/Trochita3D/Entidades/Arbol.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Tao.OpenGl;
 using Trochita3D.Core;
+using Trochita3D.Curvas;
 
 
 namespace Trochita3D.Entidades
@@ -12,96 +13,145 @@ namespace Trochita3D.Entidades
     class Arbol
     {
         private double longitudTronco;
-        private double longitudCopa;
-
+        private double radioBaseTronco;
         private PuntoFlotante[][] matrizPuntosCopa;
+
+        private static int CANTIDAD_CARAS = 15;
+        private static double FACTOR_TRONCO = 6.0f;
+        private static double PASO_BSPLINE = .1f; 
+
+        private Glu.GLUquadric quadraticCylinder = Glu.gluNewQuadric();
+
+        private static double LONGITUD_TRONCO_DEFAULT = 2.0f;
+        private static PuntoFlotante[] FORMA_COPA_DEFAULT = new PuntoFlotante[] 
+            {
+                new PuntoFlotante ( 0,   0, 0 ), // repito bordes para interpolar
+                new PuntoFlotante ( 0,   0, 0 ),
+                new PuntoFlotante ( .5,   .5, 0 ),
+                new PuntoFlotante ( .75, 1, 0 ),
+                new PuntoFlotante ( 1,   1.5, 0 ), 
+                new PuntoFlotante ( .75, 2, 0 ), 
+                new PuntoFlotante ( .5,   2.5, 0 ), 
+                new PuntoFlotante ( .6,   2.75, 0 ), 
+                new PuntoFlotante ( 0,   3, 0 ), 
+                new PuntoFlotante ( 0,   3, 0 ), // repito bordes para interpolar
+            };
+
+        public static Arbol[] GenerarArbolesAleatorios(int cantidad)
+        {
+            Arbol[] arboles = new Arbol[cantidad];
+            for (int i = 0; i < cantidad; ++i)
+            {
+                PuntoFlotante[] formaCopa = new PuntoFlotante[FORMA_COPA_DEFAULT.Length];
+                for (int j = 0; j < formaCopa.Length; ++j)
+                {
+                    formaCopa[j] = new PuntoFlotante(
+                        Arbol.FORMA_COPA_DEFAULT[j].X + Arbol.RandomEntre(-.1f, +.7f),
+                        Arbol.FORMA_COPA_DEFAULT[j].Y + Arbol.RandomEntre(-.1f, +.7f),
+                        Arbol.FORMA_COPA_DEFAULT[j].Z
+                        );
+                }
+
+                double longitudTronco = LONGITUD_TRONCO_DEFAULT + Arbol.RandomEntre(-.5f, +1.5f);
+
+                Arbol arbol = new Arbol(longitudTronco, formaCopa);
+                arboles[i] = arbol;
+            }
+            return arboles;
+        }
 
         public Arbol()
         {
-            // Default
-            PuntoFlotante[] formaCopa = new PuntoFlotante[] 
-            {
-                new PuntoFlotante ( 0, 0, 0 ),
-                new PuntoFlotante ( 1, 1, 0 ),
-                new PuntoFlotante ( 1.5, 2, 0 ),
-                new PuntoFlotante ( 2, 3, 0 ), 
-                new PuntoFlotante ( 1.5, 4, 0 ), 
-                new PuntoFlotante ( 1, 5, 0 ), 
-                new PuntoFlotante ( 0, 6, 0 ), 
-            };
-
-            this.longitudTronco = 4;
-            this.longitudCopa = 2;
-            this.matrizPuntosCopa = this.generarMatrizVertices(formaCopa, 360, 15);
+            this.longitudTronco = LONGITUD_TRONCO_DEFAULT;
+            this.radioBaseTronco = this.longitudTronco / FACTOR_TRONCO;
+            this.matrizPuntosCopa = this.generarMatrizVertices(FORMA_COPA_DEFAULT, 360.0f, CANTIDAD_CARAS);
         }
 
-        public Arbol(double longitudTronco, double longitudCopa, PuntoFlotante[] formaCopa)
+        public Arbol(double longitudTronco, PuntoFlotante[] formaCopa)
         {
             this.longitudTronco = longitudTronco;
-            this.longitudCopa = longitudCopa;
-            this.matrizPuntosCopa = this.generarMatrizVertices(formaCopa, 360, 24);
+            this.radioBaseTronco = this.longitudTronco / FACTOR_TRONCO;
+            this.matrizPuntosCopa = this.generarMatrizVertices(formaCopa, 360.0f, CANTIDAD_CARAS);
         }
 
         public void Dibujar()
-        {/*
-            Gl.glColor3f(0, 0, 1);
-            for (int i = 0; i < matrizPuntosCopa.Length; i++)
-            {
-                Gl.glBegin(Gl.GL_TRIANGLE_STRIP);
-                for (int j = 0; j < matrizPuntosCopa[i].Length; j++)
-                {
-                    Gl.glVertex3d(matrizPuntosCopa[i][j].X, matrizPuntosCopa[i][j].Y, matrizPuntosCopa[i][j].Z);
-                    System.Console.Out.WriteLine(matrizPuntosCopa[i][j].X + ", " + matrizPuntosCopa[i][j].Y + ", " + matrizPuntosCopa[i][j].Z + ") ");
+        {
+            Gl.glDisable(Gl.GL_LIGHTING);
 
-                }
-                Gl.glEnd();
-            }
-            return;*/
+            this.DibujarTronco();
 
+            Gl.glPushMatrix();
+            Gl.glTranslated(0, 0, this.longitudTronco * .8);
+            this.DibujarCopa();
+
+            Gl.glPopMatrix();
+
+            Gl.glEnable(Gl.GL_LIGHTING);
+
+        }
+
+        private void DibujarTronco()
+        {
+            // Marron
+            Gl.glColor3d(124.0f/255.0f, 87.0f/255.0f, 59.0f/255.0f);
+            Glu.gluQuadricNormals(quadraticCylinder, Glu.GLU_SMOOTH);
+            Glu.gluCylinder(quadraticCylinder, this.radioBaseTronco, this.radioBaseTronco, this.longitudTronco, CANTIDAD_CARAS, 1);
+        }
+
+        ~Arbol()
+        {
+            Glu.gluDeleteQuadric(quadraticCylinder);
+        }
+
+        private void DibujarCopa ()
+        {
+            Gl.glPushMatrix();
+
+            Gl.glRotated(90.0f, 1, 0, 0);
+            Gl.glBegin(Gl.GL_TRIANGLE_STRIP);
             for (int i = 0; i < matrizPuntosCopa.Length - 2; i++) // por columnas duplicadas
             {
-                Gl.glBegin(Gl.GL_TRIANGLE_STRIP);
                 Gl.glColor3d(0, 1, 0);
                 for (int j = 0; j < matrizPuntosCopa[i].Length; j++)
                 {
                     Gl.glNormal3d(matrizPuntosCopa[i][j].NormalX, matrizPuntosCopa[i][j].NormalY, matrizPuntosCopa[i][j].NormalZ);
                     Gl.glVertex3d(matrizPuntosCopa[i][j].X, matrizPuntosCopa[i][j].Y, matrizPuntosCopa[i][j].Z);
-                    System.Console.Out.WriteLine(matrizPuntosCopa[i][j].X + ", " + matrizPuntosCopa[i][j].Y + ", " + matrizPuntosCopa[i][j].Z + ") ");
 
                     Gl.glNormal3d(matrizPuntosCopa[i + 1][j].NormalX, matrizPuntosCopa[i + 1][j].NormalY, matrizPuntosCopa[i + 1][j].NormalZ);
                     Gl.glVertex3d(matrizPuntosCopa[i + 1][j].X, matrizPuntosCopa[i + 1][j].Y, matrizPuntosCopa[i + 1][j].Z);
-                    System.Console.Out.WriteLine(matrizPuntosCopa[i + 1][j].X + ", " + matrizPuntosCopa[i + 1][j].Y + ", " + matrizPuntosCopa[i + 1][j].Z + ") ");
-
                 }
-                Gl.glEnd();
             }
-            Gl.glEnable(Gl.GL_LIGHTING);
+            Gl.glEnd();
+            Gl.glPopMatrix();
         }
 
-        private PuntoFlotante[][] generarMatrizVertices(PuntoFlotante[] curva, double gradosRevolucion, int cantidadRotaciones)
+        private PuntoFlotante[][] generarMatrizVertices(PuntoFlotante[] curva, double gradosRevolucion, int cantidadCaras)
         {
-            double anguloRotacionRadianes = GradosARadianes(gradosRevolucion / cantidadRotaciones);
-            PuntoFlotante[][] matriz = new PuntoFlotante[cantidadRotaciones + 2][];
+            double anguloRotacionRadianes = GradosARadianes(gradosRevolucion / cantidadCaras);
+            CurvaBsplineSegmentosCubicos curvaBS = new CurvaBsplineSegmentosCubicos(curva);
+            PuntoFlotante[] curvaDiscretizada = curvaBS.GetPuntosDiscretos(PASO_BSPLINE).ToArray<PuntoFlotante>();
 
-            for (int iteradorRotacion = 0; iteradorRotacion < cantidadRotaciones; ++iteradorRotacion)
+            PuntoFlotante[][] matriz = new PuntoFlotante[cantidadCaras + 2][];
+
+            for (int iteradorRotacion = 0; iteradorRotacion < cantidadCaras; ++iteradorRotacion)
             {
-                matriz[iteradorRotacion] = new PuntoFlotante[curva.Length];
-                for (int iteradorCurva = 0; iteradorCurva < curva.Length; ++iteradorCurva)
+                matriz[iteradorRotacion] = new PuntoFlotante[curvaDiscretizada.Length];
+                for (int iteradorSpline = 0; iteradorSpline < curvaDiscretizada.Length; ++iteradorSpline)
                 {
-                    PuntoFlotante puntoCurva = curva[iteradorCurva];
-                    matriz[iteradorRotacion][iteradorCurva] = new PuntoFlotante(puntoCurva);
+                    PuntoFlotante puntoCurva = curvaDiscretizada[iteradorSpline];
+                    matriz[iteradorRotacion][iteradorSpline] = new PuntoFlotante(puntoCurva);
 
                     // roto, al mismo tiempo, los puntos de la curva alrededor del eje Y
                     PuntoFlotante puntoCurvaBack = new PuntoFlotante(puntoCurva);
                     puntoCurva.X = puntoCurvaBack.X * Math.Cos(anguloRotacionRadianes) + puntoCurvaBack.Z * Math.Sin(anguloRotacionRadianes);
                     puntoCurva.Z = puntoCurvaBack.Z * Math.Cos(anguloRotacionRadianes) - puntoCurvaBack.X * Math.Sin(anguloRotacionRadianes);
 
-                    curva[iteradorCurva] = puntoCurva;
+                    curvaDiscretizada[iteradorSpline] = puntoCurva;
                 }
             }
 
-            matriz[cantidadRotaciones] = matriz[0];
-            matriz[cantidadRotaciones + 1] = matriz[1]; // OJO solo si los grados son 360
+            matriz[cantidadCaras] = matriz[0];
+            matriz[cantidadCaras + 1] = matriz[1]; // OJO solo si los grados son 360
 
             // calculo las normales en toda la matriz
             for (int i = 0; i < matriz.Length - 1; ++i)
@@ -118,14 +168,21 @@ namespace Trochita3D.Entidades
             return matriz;
         }
 
-        private double RadianesAGrados(double radianes)
+        private static double RadianesAGrados(double radianes)
         {
             return radianes * 180.0f / Math.PI;
         }
 
-        private double GradosARadianes(double grados)
+        private static double GradosARadianes(double grados)
         {
             return grados * Math.PI / 180.0f;
+        }
+
+        private static double RandomEntre(double numMin, double numMax)
+        {
+            Random RandomNumber = new Random((int)DateTime.Now.Ticks);
+            double random = (RandomNumber.NextDouble() + numMin) % numMax;
+            return random;
         }
     }
 
