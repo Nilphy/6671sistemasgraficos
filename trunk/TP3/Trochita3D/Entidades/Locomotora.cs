934cedf6eeec005c406b440afedf74e4ec89fbef
carogalli <carogalli@07b75908-302f-5e7f-fc1a-0c613c11b170>
2010-07-11 23:02:28 +0000

    en teoría terminada la locomotora

--- trunk/TP3/Trochita3D/Entidades/Locomotora.cs
+++ trunk/TP3/Trochita3D/Entidades/Locomotora.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Tao.OpenGl;
+using Trochita3D.Entidades;
 
 namespace Trochita3D.Core
 {
@@ -40,42 +41,71 @@ namespace Trochita3D.Core
         private static double LARGO_TECHO = LARGO_RECTANULO + 0.5;
         private static double ALTO_TECHO = 0.6;
 
+        // Guardabarros
+        private static double ALTO_GUARDABARROS = ALTO_PARAGOLPE;
+        private static double LARGO_GUARDABARROS = ANCHO_BASE / 2.5d;
+        private static double ANCHO_GUARDABARROS = ANCHO_PARAGOLPE;
+
+        // Ruedas
+        private static double ESPACIADO_RUEDAS = ANCHO_BASE / 2d; // Esto tiene que ir con la distancia entre los rieles
+        private static double RADIO_INTERNO_RUEDAS = (ALTO_PARAGOLPE + ALTO_GUARDABARROS) / 2d;
+        private static double RADIO_EXTERNO_RUEDAS = RADIO_INTERNO_RUEDAS + 0.1d;
+        private static double ANCHO_RUEDAS = 0.8d;
+
         #endregion
-
-        // Todo, agrega inclinación
-        public Punto PosicionLocomotora { set; get; }
-
         #region Partes de la locomotora
 
         private Cuboide rectanguloTechoConductor;
         private Cuboide rectanguloConductor;
         private Cuboide rectanguloBase;
         private Cuboide rectanguloParagolpe;
+        private IList<Rueda> ruedas;
+        private GuardabarroTren guardabarroTren;
+        
+        #endregion
+
+        private double AnguloRotacionRuedas { set; get; }
+        public double InclinaciónLocomotora { set; get; }
+        public float[] LuzAmbiente { set; get; }
+        public float[] LuzBrillo { set; get; }
+        public float[] Luz { set; get; }
+        public int Shininess { set; get; }
+
+        public Punto Posicion { set; get; }
+        public double AnguloRotacion { set; get; }
 
         #endregion
 
-
-        #endregion
-
-        /// <summary>
-        /// Se debe construir estáticamente porque acá se crea la 
-        /// display list, despues se va redibujando en el paint
-        /// en cada simulación
-        /// </summary>
-        public Train()
+        public Train(float[] luzAmbiente, float[] luzBrillo, float[] luz, int shininess)
         {
-            this.PosicionLocomotora = new Punto(0, 0, 0);
+            this.Luz = luz;
+            this.LuzAmbiente = luzAmbiente;
+            this.LuzBrillo = luzBrillo;
+            this.Shininess = shininess;
 
-            rectanguloTechoConductor = new Cuboide(ANCHO_TECHO, LARGO_TECHO, ALTO_TECHO, 32, 32, 32, this.PosicionLocomotora.SumarPunto(new Punto(-ANCHO_TECHO/2d, -0.25, ALTO_RECTANGULO)));
-            rectanguloBase = new Cuboide(ANCHO_BASE, LARGO_BASE, ALTO_BASE, 32, 32, 32, this.PosicionLocomotora.SumarPunto(new Punto(-ANCHO_BASE / 2d, 0, -ALTO_BASE)));
-            rectanguloConductor = new Cuboide(ANCHO_RECTANGULO, LARGO_RECTANULO, ALTO_RECTANGULO, 32, 32, 32, this.PosicionLocomotora.SumarPunto(new Punto(-ANCHO_RECTANGULO / 2d, 0, 0)));
-            rectanguloParagolpe = new Cuboide(ANCHO_PARAGOLPE, LARGO_PARAGOLPE, ALTO_PARAGOLPE, 32, 32, 32, this.PosicionLocomotora.SumarPunto(new Punto(-ANCHO_PARAGOLPE/2d, LARGO_BASE, - ALTO_PARAGOLPE)));
+            rectanguloTechoConductor = new Cuboide(ANCHO_TECHO, LARGO_TECHO, ALTO_TECHO, 32, 32, 32, new Punto(-ANCHO_TECHO/2d, -0.25, ALTO_RECTANGULO), this.LuzAmbiente, this.LuzBrillo, this.Luz, this.Shininess);
+            rectanguloBase = new Cuboide(ANCHO_BASE, LARGO_BASE, ALTO_BASE, 32, 32, 32, new Punto(-ANCHO_BASE / 2d, 0, -ALTO_BASE), this.LuzAmbiente, this.LuzBrillo, this.Luz, this.Shininess);
+            rectanguloConductor = new Cuboide(ANCHO_RECTANGULO, LARGO_RECTANULO, ALTO_RECTANGULO, 32, 32, 32, new Punto(-ANCHO_RECTANGULO / 2d, 0, 0), this.LuzAmbiente, this.LuzBrillo, this.Luz, this.Shininess);
+            rectanguloParagolpe = new Cuboide(ANCHO_PARAGOLPE, LARGO_PARAGOLPE, ALTO_PARAGOLPE, 32, 32, 32, new Punto(-ANCHO_PARAGOLPE / 2d, LARGO_BASE, -ALTO_PARAGOLPE), this.LuzAmbiente, this.LuzBrillo, this.Luz, this.Shininess);
+
+            ruedas = new List<Rueda>();
+
+            ruedas.Add(new Rueda(this.CalcularPuntoCentroRueda(true, true), AnguloRotacionRuedas, RADIO_INTERNO_RUEDAS, RADIO_EXTERNO_RUEDAS, ANCHO_RUEDAS, this.LuzAmbiente, this.LuzBrillo, this.Luz, this.Shininess));
+            ruedas.Add(new Rueda(this.CalcularPuntoCentroRueda(true, false), AnguloRotacionRuedas, RADIO_INTERNO_RUEDAS, RADIO_EXTERNO_RUEDAS, ANCHO_RUEDAS, this.LuzAmbiente, this.LuzBrillo, this.Luz, this.Shininess));
+            ruedas.Add(new Rueda(this.CalcularPuntoCentroRueda(false, true), AnguloRotacionRuedas, RADIO_INTERNO_RUEDAS, RADIO_EXTERNO_RUEDAS, ANCHO_RUEDAS, this.LuzAmbiente, this.LuzBrillo, this.Luz, this.Shininess));
+            ruedas.Add(new Rueda(this.CalcularPuntoCentroRueda(false, false), AnguloRotacionRuedas, RADIO_INTERNO_RUEDAS, RADIO_EXTERNO_RUEDAS, ANCHO_RUEDAS, this.LuzAmbiente, this.LuzBrillo, this.Luz, this.Shininess));
+
+            guardabarroTren = new GuardabarroTren(ANCHO_GUARDABARROS, LARGO_GUARDABARROS, ALTO_GUARDABARROS, 32, 32, 32, new Punto(-ANCHO_GUARDABARROS / 2d, LARGO_BASE + LARGO_PARAGOLPE, -ALTO_PARAGOLPE - ALTO_GUARDABARROS + 0.2), this.Luz, this.LuzAmbiente, this.LuzBrillo, this.Shininess);
         }
 
         #region Dibujadores
 
         public void Draw()
         {
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+            Gl.glTranslated(this.Posicion.X, this.Posicion.Y, this.Posicion.Z);
+            Gl.glRotated(this.AnguloRotacion, 0, 0, 1);
+
             // Techo del conductor
             this.rectanguloTechoConductor.Draw();
 
@@ -95,15 +125,18 @@ namespace Trochita3D.Core
             this.DrawAgarradores();
 
             // Ruedas
-            // this.DrawRuedas();
+            this.DrawRuedas();
 
             // Triangulo de adelante
-            
+            this.guardabarroTren.Draw();
         }
 
         private void DrawRuedas()
         {
-            throw new NotImplementedException();
+            foreach (Rueda rueda in ruedas)
+            {
+                rueda.Draw();
+            }
         }
 
         private void DrawAgarradores()
@@ -122,9 +155,10 @@ namespace Trochita3D.Core
             Gl.glTranslated((esIzquierdo ? -1 : 1) * ANCHO_RECTANGULO / 3d, LARGO_BASE + LARGO_PARAGOLPE + (LARGO_AGARRADOR / 2d), -RADIO_AGARRADOR);
             Gl.glRotated(90, 1, 0, 0);
             
-            Gl.glMaterialfv(Gl.GL_FRONT_AND_BACK, Gl.GL_AMBIENT, new float[] { 0.20f, 0.20f, 0.35f, 1 });
-            Gl.glMaterialfv(Gl.GL_FRONT_AND_BACK, Gl.GL_DIFFUSE, new float[] { 0.2f, 0.2f, 0.3f, 1 });
-            Gl.glMaterialfv(Gl.GL_FRONT_AND_BACK, Gl.GL_SPECULAR, new float[] { 0.5f, 0.5f, 0.5f, 1 });
+            Gl.glMaterialfv(Gl.GL_FRONT, Gl.GL_AMBIENT, this.LuzAmbiente);
+            Gl.glMaterialfv(Gl.GL_FRONT, Gl.GL_DIFFUSE, this.Luz);
+            Gl.glMaterialfv(Gl.GL_FRONT, Gl.GL_SPECULAR, this.LuzBrillo);
+            Gl.glMateriali(Gl.GL_FRONT, Gl.GL_SHININESS, this.Shininess);
 
             Glu.gluCylinder(quad, RADIO_AGARRADOR, RADIO_AGARRADOR, LARGO_AGARRADOR, 20, 20);
             Gl.glRotated(180, 1, 0, 0);
@@ -150,9 +184,10 @@ namespace Trochita3D.Core
             Gl.glTranslated(0, ANCHO_RECTANGULO + (LARGO_TROMPA / 2d), RADIO_TROMPA);
             Gl.glRotated(90, 1, 0, 0);
 
-            Gl.glMaterialfv(Gl.GL_FRONT_AND_BACK, Gl.GL_AMBIENT, new float[] { 0.20f, 0.20f, 0.35f, 1 });
-            Gl.glMaterialfv(Gl.GL_FRONT_AND_BACK, Gl.GL_DIFFUSE, new float[] { 0.2f, 0.2f, 0.3f, 1 });
-            Gl.glMaterialfv(Gl.GL_FRONT_AND_BACK, Gl.GL_SPECULAR, new float[] { 0.5f, 0.5f, 0.5f, 1 });
+            Gl.glMaterialfv(Gl.GL_FRONT, Gl.GL_AMBIENT, this.LuzAmbiente);
+            Gl.glMaterialfv(Gl.GL_FRONT, Gl.GL_DIFFUSE, this.Luz);
+            Gl.glMaterialfv(Gl.GL_FRONT, Gl.GL_SPECULAR, this.LuzBrillo);
+            Gl.glMateriali(Gl.GL_FRONT, Gl.GL_SHININESS, this.Shininess);
 
             Glu.gluCylinder(quad, RADIO_TROMPA, RADIO_TROMPA, LARGO_TROMPA, 20, 20);
             Gl.glRotated(180, 1, 0, 0);
@@ -162,14 +197,27 @@ namespace Trochita3D.Core
             Gl.glDisable(Gl.GL_LIGHTING);
             Gl.glPopMatrix();
         }
-
         
         #endregion
         #region utilitarios
 
+        private Punto CalcularPuntoCentroRueda(bool esDerecha, bool esDelantera)
+        {
+            double coordenadaX;
+            double coordenadaY;
+            double coordenadaZ;
+
+            coordenadaZ = -(RADIO_EXTERNO_RUEDAS + ALTO_BASE);
+
+            if (esDerecha) coordenadaX = (ESPACIADO_RUEDAS / 2d);
+            else coordenadaX = - ((ESPACIADO_RUEDAS / 2d) + ANCHO_RUEDAS);
+
+            if (esDelantera) coordenadaY = (LARGO_BASE / 4d) * 3d;
+            else coordenadaY = LARGO_BASE / 4d;
+
+            return new Punto(coordenadaX, coordenadaY, coordenadaZ); 
+        }
+
         #endregion
     }
 }
-
-
-
---
+++ trunk/TP3/Trochita3D/Entidades/Rueda.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Trochita3D.Core;
+using Tao.OpenGl;
+
+namespace Trochita3D.Entidades
+{
+    public class Rueda
+    {
+        public Punto Centro { set; get; }
+        public Double AnguloRotacion { set; get; }
+        public Double RadioInterno { set; get; }
+        public Double RadioExterno { set; get; }
+        public Double Ancho { set; get; }
+        public float[] Luz { set; get; }
+        public float[] LuzAmbiente { set; get; }
+        public float[] LuzBrillo { set; get; }
+        public int Shininess { set; get; }
+
+        public Rueda(
+            Punto centro, 
+            Double anguloRotacion, 
+            double radioInterno, 
+            double radioExterno, 
+            double ancho,
+            float[] luz,
+            float[] luzBrillo, 
+            float[] luzAmbiente,
+            int shininess)
+        {
+            this.Centro = centro;
+            this.AnguloRotacion = anguloRotacion;
+            this.RadioExterno = radioExterno;
+            this.RadioInterno = radioInterno;
+            this.Luz = luz;
+            this.LuzAmbiente = luzAmbiente;
+            this.LuzBrillo = luzBrillo;
+            this.Ancho = ancho;
+            this.Shininess = shininess;
+        }
+
+        public void Draw()
+        {
+            Glu.GLUquadric quad = Glu.gluNewQuadric();
+                        
+            Gl.glPushMatrix();
+            Gl.glEnable(Gl.GL_LIGHTING);
+
+            Gl.glMaterialfv(Gl.GL_FRONT, Gl.GL_AMBIENT, LuzAmbiente);
+            Gl.glMaterialfv(Gl.GL_FRONT, Gl.GL_DIFFUSE, Luz);
+            Gl.glMaterialfv(Gl.GL_FRONT, Gl.GL_SPECULAR, LuzBrillo);
+            Gl.glMateriali(Gl.GL_FRONT, Gl.GL_SHININESS, this.Shininess);
+
+            Gl.glTranslated(this.Centro.X, this.Centro.Y, this.Centro.Z);
+            Gl.glRotated(this.AnguloRotacion * 180 / Math.PI, 1, 0, 0);
+            Gl.glRotated(90, 0, 1, 0);
+            
+            Glu.gluCylinder(quad, this.RadioExterno, this.RadioExterno, Ancho, 20, 20);
+
+            Gl.glRotated(180, 0, 1, 0);   
+
+            Glu.gluDisk(quad, this.RadioInterno, this.RadioExterno, 20, 20);
+            Glu.gluDisk(quad, 0, this.RadioInterno, 20, 20);
+
+            Gl.glRotated(180, 0, 1, 0);   
+
+            Gl.glTranslated(0, 0, Ancho);
+         
+            Glu.gluDisk(quad, this.RadioInterno, this.RadioExterno, 20, 20);                        
+            Glu.gluDisk(quad, 0, this.RadioInterno, 20, 20);
+
+            this.DibujarBarra();
+
+            Gl.glDisable(Gl.GL_LIGHTING);
+            Gl.glPopMatrix();
+            Glu.gluDeleteQuadric(quad);
+        }
+
+        private void DibujarBarra()
+        {
+            Gl.glColor3d(0, 0, 1);
+            double size = this.RadioExterno;
+            double height = size;
+            double width = size / 2d;
+            double depth = size / 2d;
+
+            Gl.glMaterialfv(Gl.GL_FRONT, Gl.GL_AMBIENT, new float[] { 0.3f, 0.2f, 0.1f, 1 });
+            Gl.glMaterialfv(Gl.GL_FRONT, Gl.GL_DIFFUSE, new float[] { 0.3f, 0.2f, 0.1f, 1 });
+            Gl.glMaterialfv(Gl.GL_FRONT, Gl.GL_SPECULAR, new float[] { 0.2f, 0.2f, 0.2f, 1 });
+            Gl.glMateriali(Gl.GL_FRONT, Gl.GL_SHININESS, 10);
+
+            // Centro la barra.
+            Gl.glTranslated(-depth / 2d, -width / 2d, - (Ancho/2d) - (height /2d));
+
+            Gl.glBegin(Gl.GL_QUADS);
+
+            Gl.glNormal3f(0, 0, -1);
+
+            // Tapa de abajo
+            Gl.glVertex3d(0, 0, 0);
+            Gl.glVertex3d(depth, 0, 0);
+            Gl.glVertex3d(depth, width, 0);
+            Gl.glVertex3d(0, width, 0);
+
+            Gl.glNormal3f(0, 0, 1);
+
+            // Tapa de arriba
+            Gl.glVertex3d(0, 0, height);
+            Gl.glVertex3d(depth, 0, height);
+            Gl.glVertex3d(depth, width, height);
+            Gl.glVertex3d(0, width, height);
+
+            Gl.glNormal3f(-1, 0, 0);
+
+            // Tapa de atras
+            Gl.glVertex3d(0, 0, 0);
+            Gl.glVertex3d(0, width, 0);
+            Gl.glVertex3d(0, width, height);
+            Gl.glVertex3d(0, 0, height);
+
+            Gl.glNormal3f(0, 1, 0);
+
+            // Tapa de derecha
+            Gl.glVertex3d(0, width, 0);
+            Gl.glVertex3d(depth, width, 0);
+            Gl.glVertex3d(depth, width, height);
+            Gl.glVertex3d(0, width, height);
+
+            Gl.glNormal3f(0, 1, 0);
+
+            // Tapa de adelante
+            Gl.glVertex3d(depth, 0, 0);
+            Gl.glVertex3d(depth, width, 0);
+            Gl.glVertex3d(depth, width, height);
+            Gl.glVertex3d(depth, 0, height);
+
+            Gl.glNormal3f(0, 1, 0);
+
+            // Tapa de izquierda
+            Gl.glVertex3d(0, 0, 0);
+            Gl.glVertex3d(depth, 0, 0);
+            Gl.glVertex3d(depth, 0, height);
+            Gl.glVertex3d(0, 0, height);
+            
+            Gl.glEnd();
+        }
+    }
+}
